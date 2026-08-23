3b2d2845a64f0566683c921d01c18f6d73bf143a
alokjaiswal2910-art <alokjaiswal2910@gmail.com>
2026-08-23 11:06:03 +0530

    Time: 51 ms (41.92%) | Memory: 45.3 MB (70.94%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
+    
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0;
-        int sum = 0;
-        
-        // Map to store frequency of prefix sums
-        std::unordered_map<int, int> sumFreq;
-        
-        // Base case: a prefix sum of 0 has occurred once
-        sumFreq[0] = 1;
-        for (int i=0;i<nums.size();i++){
+        unordered_map<int,int> f;
+        int sum=0;
+        int count=0;
+        f[0]=1;
+        for (int i =0;i<nums.size();i++){
             sum+=nums[i];
-            int que= sum -k;
-            if (sumFreq.find(que)!=sumFreq.end()){
-                count+=sumFreq[que];
+            int que=sum-k;
+            if(f.find(que)!=f.end()){
+                count+=f[que];
             }
-            sumFreq[sum]++;
+            f[sum]++;
         }
-        
-        
-           
-            
-        
-        
         return count;
     }
+
+
 
         
     
