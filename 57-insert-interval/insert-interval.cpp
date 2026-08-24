c39cb8f3fce47dbe4b06911228e31bbb272b05a2
alokjaiswal2910-art <alokjaiswal2910@gmail.com>
2026-08-24 13:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (95.25%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        intervals.push_back(newInterval);
         vector<vector<int>> res;
-        sort(intervals.begin(),intervals.end());
-        int start1=intervals[0][0];
-        int end1=intervals[0][1];
+        int i = 0, n = intervals.size();
 
-        for(int i=1;i<intervals.size();i++){
-            int start2=intervals[i][0];
-            int end2=intervals[i][1];
-            if(end1>=start2){
-                start1=start1;
-                end1=max(end1,end2);
-                continue;
-            }
-            res.push_back({start1,end1});
-            start1=start2;
-            end1=end2;
-    
+        // Step 1: Add all intervals before newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
         }
-        res.push_back({start1,end1});
+
+        // Step 2: Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.push_back(newInterval);
+
+        // Step 3: Add remaining intervals
+        while (i < n) {
+            res.push_back(intervals[i]);
+            i++;
+        }
+
         return res;
-        
-        
-        
     }
 };
